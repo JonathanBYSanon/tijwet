446ed864b00f725727b2b3e3b95b761458c58d43
jonathan sanon <jonathansanon1999@gmail.com>
2025-03-22 21:58:04 -0400

    - Addition of info pages, - Addition of the fullscreen option - Addition of the searchbar - 404 page and CNAME - Addition of a tijwet logo

--- JS/header-footer.js
+++ JS/header-footer.js
@@ -1,3 +1,17 @@
+let footerLinks =
+[
+    {
+        "name": "Legal",
+        "url": "legal.html",
+        "folder": "INFO"
+    },
+    {
+        "name": "About",
+        "url": "about.html",
+        "folder": "INFO"
+    }
+];
+
 window.addEventListener("load", () => {
     document.body.classList.add("visible");
   });
@@ -11,6 +25,12 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(res => res.text())
         .then(html => {
             document.body.insertAdjacentHTML("afterbegin", html);
+            const headerImg = document.querySelector('header img');
+            if (headerImg) {
+                headerImg.src = isIndex
+                    ? "ASSETS/logo-TiJwet-white.svg"
+                    : "../../ASSETS/logo-TiJwet-white.svg";
+            }
         });
 
     fetch(`${basePath}COMPONENT/footer.html`)
@@ -24,6 +44,37 @@ document.addEventListener("DOMContentLoaded", function () {
                     ? "ASSETS/logo_jonathan_sanon.svg"
                     : "../../ASSETS/logo_jonathan_sanon.svg";
             }
+
+            let footerSpan = document.querySelector("footer p span");
+            footerLinks.forEach(link => {
+                const a = document.createElement("a");
+                a.href = isIndex
+                    ? `HTML/${link.folder}/${link.url}`
+                    : `../${link.folder}/${link.url}`;
+                    a.textContent = link.name;
+                
+                footerSpan.appendChild(document.createTextNode(" · "));
+                footerSpan.appendChild(a);
+            });
         });
     
 });
+
+
+const toggleBtn = document.getElementById("fullscreen-toggle");
+
+if (toggleBtn) {
+  const icon = toggleBtn.querySelector("i");
+
+  toggleBtn.addEventListener("click", () => {
+    const header = document.querySelector("header");
+    const footer = document.querySelector("footer");
+    const isFullscreen = header?.classList.contains("hidden");
+
+    header?.classList.toggle("hidden");
+    footer?.classList.toggle("hidden");
+
+    // Change icon
+    icon.className = isFullscreen ? "fas fa-expand" : "fas fa-compress";
+  });
+}
--- JS/home.js
+++ JS/home.js
@@ -2,12 +2,14 @@ let games = [
     {
         "img" : "ASSETS/tic-tac-toe.webp",
         "title" : "Tic Tac Toe",
-        "url" : "HTML/GAME/tic-tac-toe.html"
+        "url" : "HTML/GAME/tic-tac-toe.html",
+        "default" : false
     },
     {
         "img" : "ASSETS/more_coming_soon.webp",
         "title" : "MORE COMING SOON",
-        "url" : "#"
+        "url" : "#",
+        "default" : true
     },
 ];
 
@@ -22,6 +24,29 @@ games.forEach(game => {
     img.src = game.img;
     img.alt = game.title;
     game_clone.querySelector('h2').textContent = game.title;
-    game_clone.querySelector('a').href = game.url;
+    let a = game_clone.querySelector('a');
+    a.href = game.url;
+
+    if (game.default) {
+        game_clone.querySelector('.game').classList.add('default');
+        a.textContent = "+";
+    }
+
     body_main.appendChild(game_clone);
 });
+
+
+const searchInput = document.getElementById("game-search");
+
+if (searchInput) {
+  searchInput.addEventListener("input", () => {
+    const searchTerm = searchInput.value.toLowerCase();
+    const gamesUi = document.querySelectorAll("main .game:not(.default)");
+
+    gamesUi.forEach(game => {
+      const title = game.querySelector(".game-title")?.textContent.toLowerCase() || "";
+      const match = title.includes(searchTerm);
+      game.style.display = match ? "grid" : "none";
+    });
+  });
+}
